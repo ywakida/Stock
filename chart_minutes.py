6c885867aed8c9b44946bee3a27897f92777d1ea
ywakida <122138872+ywakida@users.noreply.github.com>
2023-03-11 18:42:42 +0900

    Update and rename chart1d.py to chart_minutes.py

--- chart_minutes.py
+++ chart_minutes.py
@@ -21,7 +21,7 @@ def task():
           os.makedirs(folder, exist_ok=True) 
           chart_savename = f'{folder}/{ticker}_1m.csv'          
           chart1m = pandas.DataFrame()
-          chart1m = yfinance.download(tickers=f'{ticker}.T', period='1d', interval='1m', progress=False)
+          chart1m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='1m', progress=False)
           if not chart1m.empty:
             if len(chart1m)>1:
               chart1m.to_csv(chart_savename)
@@ -30,7 +30,7 @@ def task():
           os.makedirs(folder, exist_ok=True) 
           chart_savename = f'{folder}/{ticker}_5m.csv'
           chart5m = pandas.DataFrame()        
-          chart5m = yfinance.download(tickers=f'{ticker}.T', period='1d', interval='5m', progress=False)
+          chart5m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='5m', progress=False)
           if not chart5m.empty:
             if len(chart5m)>1:
               chart5m.to_csv(chart_savename)
