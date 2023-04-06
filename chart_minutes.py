fb545b43d6800af4992f634c2d0e35ff7ab2505d
ywakida <122138872+ywakida@users.noreply.github.com>
2023-04-07 00:58:21 +0900

    Update chart_minutes.py

--- chart_minutes.py
+++ chart_minutes.py
@@ -14,30 +14,33 @@ def task():
     tickers_file = pandas.read_csv(csvfile, header=0, index_col=0)
     print('start:', datetime.datetime.now())
     for ticker, row in tickers_file.iterrows():
-        print('ticker: ', ticker)
+        
+        folder = f'{basepath}chart1m1d'
+        os.makedirs(folder, exist_ok=True) 
+        chart_savename = f'{folder}/{ticker}.csv'          
+        chart1m = pandas.DataFrame()
         try:
-          folder = f'{basepath}chart1m1d'
-          os.makedirs(folder, exist_ok=True) 
-          chart_savename = f'{folder}/{ticker}.csv'          
-          chart1m = pandas.DataFrame()
-          chart1m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='1m', progress=False)
-          if not chart1m.empty:
-            if len(chart1m)>1:
-              chart1m.to_csv(chart_savename)
-              print(chart_savename)
-
-          folder = f'{basepath}chart5m1d'
-          os.makedirs(folder, exist_ok=True) 
-          chart_savename = f'{folder}/{ticker}.csv'
-          chart5m = pandas.DataFrame()        
-          chart5m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='5m', progress=False)
-          if not chart5m.empty:
-            if len(chart5m)>1:
-              chart5m.to_csv(chart_savename)
-              print(chart_savename)
-          
+            chart1m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='1m', progress=False)
         except Exception:
-          pass
+            pass 
+        
+        if not chart1m.empty:
+            if len(chart1m)>1:
+                chart1m.to_csv(chart_savename)
+
+        folder = f'{basepath}chart5m1d'
+        os.makedirs(folder, exist_ok=True) 
+        chart_savename = f'{folder}/{ticker}.csv'
+        chart5m = pandas.DataFrame()        
+        try:
+            chart5m = yfinance.download(tickers=f'{ticker}.T', period='5d', interval='5m', progress=False)
+        except Exception:
+            pass
+        
+        if not chart5m.empty:
+          if len(chart5m)>1:
+            chart5m.to_csv(chart_savename)
+          
 
     print('end:', datetime.datetime.now())
     
